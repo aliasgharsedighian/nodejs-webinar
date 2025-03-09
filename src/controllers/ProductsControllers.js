b99d752efc738ed23e18257424f5f7de9a913874
ali asghar sedighian linux co <as.sedighian@gmail.com>
2025-03-09 13:07:15 +0330

    handle error on products controller

--- src/controllers/ProductsControllers.js
+++ src/controllers/ProductsControllers.js
@@ -1,12 +1,22 @@
 import { client } from "../config/db.pgsql.js";
 
+// @access  Public
 export const getAllProducts = async (request, response, next) => {
   try {
-    const res = await client.query("SELECT * from product");
-    return response.status(200).json({
-      status: 200,
+    const products = await client.query("SELECT * from product");
+    if (!products) {
+      const statusCode = 404;
+      return response.status(statusCode).json({
+        status: statusCode,
+        message: "products not found",
+        data: [],
+      });
+    }
+    const statusCode = 200;
+    return response.status(statusCode).json({
+      status: statusCode,
       message: "get all products successfully",
-      data: res.rows,
+      data: products.rows,
     });
   } catch (error) {
     return response.status(500).json({
@@ -18,16 +28,23 @@ export const getAllProducts = async (request, response, next) => {
 
 export const getSingleProduct = async (request, response, next) => {
   try {
-    const productSlug = request.params.slug;
-
-    const res = await client.query(
-      `SELECT * from product where id = ${productSlug}`
+    const productId = request.params.slug;
+    const product = await client.query(
+      `SELECT * from product where id = ${productId}`
     );
+    if (!product || product.rows.length === 0) {
+      const statusCode = 404;
+      return response.status(statusCode).json({
+        status: statusCode,
+        message: "product not found",
+        data: {},
+      });
+    }
 
     return response.status(200).json({
       status: 200,
       message: "get product successfully",
-      data: res.rows[0],
+      data: product.rows[0],
     });
   } catch (error) {
     return response.status(500).json({
@@ -36,7 +53,7 @@ export const getSingleProduct = async (request, response, next) => {
     });
   }
 };
-
+// @access  Private/Admin
 export const addProduct = async (request, response, next) => {
   try {
   } catch (error) {
