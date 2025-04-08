68782bacbabb4344a12d0706afd261446878abe3
ali asghar sedighian linux co <as.sedighian@gmail.com>
2025-04-08 10:28:39 +0330

    handle database error & not found route

--- index.ts
+++ index.ts
@@ -1,25 +1,58 @@
 import express from "express";
 import dotenv from "dotenv";
 import V1Router from "./src/routes/routes";
+import { connectToDatabase } from "./src/prisma";
 
+// Load environment variables
 dotenv.config();
 
 const port = process.env.PORT || 4001;
 
+// Check if MONGODB_URI is loaded
+if (!process.env.POSTGRES_DATABASE_URL) {
+  console.error("Error: POSTGRES_DATABASE_URL is not defined in .env");
+  process.exit(1);
+}
+
+// Initialize Express app
 const app = express();
 
 //Body parsers
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use("/api/v1", V1Router);
-// Non-exist get pages handler
+
+// Connect to Postgresql
+connectToDatabase();
+
+// Non-exist get and post pages handler
 app.get("*", (req, res) => {
   res.status(404).json({
     status: 404,
     message: "Page Not Found",
   });
 });
+app.post("*", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: "Route Not Found",
+  });
+});
 
 app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+  var d = new Date();
+  var utcDate = new Date(
+    Date.UTC(
+      d.getUTCFullYear(),
+      d.getUTCMonth(),
+      d.getUTCDate(),
+      d.getUTCHours(),
+      d.getUTCMinutes(),
+      d.getUTCSeconds(),
+      d.getUTCMilliseconds()
+    )
+  );
+  console.log(`app running on port ${port} on ${utcDate}`);
 });
--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
+
+// Function to connect to the database
+export async function connectToDatabase() {
+  try {
+    await prisma.$connect();
+    console.log("🟢 Connected to the database successfully");
+  } catch (error) {
+    console.error("🔴 Failed to connect to the database:", error);
+    process.exit(1); // Exit the process if unable to connect
+  }
+
+  // Handle graceful shutdown
+  process.on("SIGINT", async () => {
+    await prisma.$disconnect();
+    console.log("🔌 Database connection closed");
+    process.exit(0);
+  });
+}
+
 export default prisma;
